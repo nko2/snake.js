9294b3b6e7d64bed90d277fe309392cc45a66ee6
Charlie Yuan <charlie@klout.com>
2011-08-27 12:18:15 -0700

    app.js formatting

--- app.js
+++ app.js
@@ -6,29 +6,27 @@ var http = require('http'),
     io = require('socket.io').listen(app);
 
 app.configure(function(){
-  app.use(express.static(__dirname + '/static'));
+    app.use(express.static(__dirname + '/static'));
 });
 
 app.listen(process.env.NODE_ENV === 'production' ? 80 : 8000, function() {
-  console.log('Ready');
-
-  // if run as root, downgrade to the owner of this file
-  if (process.getuid() === 0)
-    require('fs').stat(__filename, function(err, stats) {
-      if (err) return console.log(err)
-      process.setuid(stats.uid);
+    // if run as root, downgrade to the owner of this file
+    if (process.getuid() === 0)
+        require('fs').stat(__filename, function(err, stats) {
+        if (err) return console.log(err)
+            process.setuid(stats.uid);
     });
 });
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+    res.sendfile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
 });
 
 console.log('Listening on ' + app.address().port);
