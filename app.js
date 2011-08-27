224c1b457ee80bf276363f3de99e0eea424cde43
Charlie Yuan <charlie@klout.com>
2011-08-27 11:51:33 -0700

    add back 80 8080 logic

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ var http = require('http'),
 var app = require('express').createServer()
   , io = require('socket.io').listen(app);
 
-app.listen(8080);
+app.listen(process.env.NODE_ENV === 'production' ? 80 : 8000, function() {
+  console.log('Ready');
+
+  // if run as root, downgrade to the owner of this file
+  if (process.getuid() === 0)
+    require('fs').stat(__filename, function(err, stats) {
+      if (err) return console.log(err)
+      process.setuid(stats.uid);
+    });
+});
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
