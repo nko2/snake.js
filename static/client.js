a48297f2b14632078ec011a68594a8d30413bbf2
Mindy Weng <tzumingweng@gmail.com>
2011-08-27 17:08:01 -0700

    clearamp

--- static/client.js
+++ static/client.js
@@ -3,29 +3,41 @@
         console.log("you are good to go");
         $(document).ready(function(){
             // initialize canvas
-            var canvasBlocks = {x: 80, y:60, width:9, height:9};
+            var canvasBlocks = {X_MAXLENGTH: 80, Y_MAXLENGTH:60, width:9, height:9};
             var canvas = $('#canvas')[0];
             var context = canvas.getContext('2d');
-            var x, y;
-
+            var posX = 0, posY = 1;
+            
+            // draw one snake block
+            var drawBlock = function(x, y){
+                context.fillRect(x, y, 9, 9);
+            }
+            
             // draw the default gray playground 
-            context.fillStyle = "#efefef";
-            for(x = 0; x < canvasBlocks.x; x++){
-                for(y=0; y < canvasBlocks.y; y++){
-                    context.fillRect(10*x,10*y, 9, 9);
+            var clearMap = function(){
+                var x, y;
+                context.fillStyle = "#efefef";
+                for(x = 0; x < canvasBlocks.X_MAXLENGTH; x++){
+                    for(y = 0; y < canvasBlocks.Y_MAXLENGTH; y++){
+                        drawBlock(10*x, 10*y);
+                    }
                 }
             }
+            clearMap();
+            
             // update the wall
             var updateMap = function(walls){
                 context.fillStyle = "#000";
                 $(walls).each(function(index, elem){
-                    context.fillRect(10*elem[0], 10*elem[1], 9, 9);
+                    drawBlock(10*block[posX], 10*block[poxY]);
                 });
             };
+
+            // draw one snake
             var drawOneSnake = function(snake){
                 context.fillStyle = snake.color;
                 $(snake.body).each(function(index, block){
-                    context.fillRect(10*block[0], 10*block[1], 9, 9);
+                    drawBlock(10*block[posX], 10*block[posY]);
                 });
             }
             // draw/update snakes
@@ -35,20 +47,14 @@
                     drawOneSnake(snake);
                 });
             }
+
             // connect
             var socket = io.connect("/");
             socket.on('game state', function(data){
                 console.log(data);
+                clearMap();
                 drawSnakes(data.snakes);
-                //updateMap(data.walls);
             });
-            
-/*
-            var snakeSample1 = {id:"1", color:"red", head: [50, 50], body:[[51,50],[52,50], [53,50]], name:"mindy", state:"alive"}; 
-            var snakeSample2 = {id:"2", color:"blue", head:[60, 60], body:[[61,50],[62,50]], name:"charlie", state:"alive"}; 
-            var snakes = [snakeSample1, snakeSample2];
-            drawSnakes(snakes);
-*/
             // key events
             $(document).keydown(function(event){
                 var keycode = event.keyCode?event.keyCode: event.which;
