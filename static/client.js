3c7bcb8f2408a6efd4664ec9143db01f77a20c84
Mindy Weng <tzumingweng@gmail.com>
2011-08-27 19:39:02 -0700

    beautify the background of index.html

--- static/client.js
+++ static/client.js
@@ -4,7 +4,7 @@
         $(document).ready(function(){
             // initialize canvas
             var canvasBlocks = {X_MAXLENGTH: 80, Y_MAXLENGTH:60, width:9, height:9};
-            var canvas = $('#canvas')[0];
+            var canvas = $('canvas#canvas')[0];
             var context = canvas.getContext('2d');
             var posX = 0, posY = 1;
             
@@ -12,11 +12,23 @@
             var drawBlock = function(x, y){
                 context.fillRect(x, y, 9, 9);
             }
+            /*
+            var snakeLogo = (function(){
+                var headerCanvas = $("#header")[0];
+                var headerContext = headerCanvas.getContext('2d');
+                var x, y;
+                headerContext.fillStyle = "#ccc";
+                for(x = 0; x < 110; x++){
+                    for(y = 0; y < 10; y++){
+                        headerContext.fillRect(10*x, 10*y, 9, 9);
+                    }
+                }
+            })();*/
             
             // draw the default gray playground 
             var clearMap = function(){
                 var x, y;
-                context.fillStyle = "#efefef";
+                context.fillStyle = "#dedede";
                 for(x = 0; x < canvasBlocks.X_MAXLENGTH; x++){
                     for(y = 0; y < canvasBlocks.Y_MAXLENGTH; y++){
                         drawBlock(10*x, 10*y);
@@ -36,9 +48,16 @@
             // draw one snake
             var drawOneSnake = function(snake){
                 context.fillStyle = snake.color;
-                $(snake.body).each(function(index, block){
-                    drawBlock(10*block[posX], 10*block[posY]);
-                });
+                if(snake.state === "baby"){
+                    $(snake.body).each(function(index, block){
+
+                        drawBlock(10*block[posX], 10*block[posY]);
+                    });
+                }else if(snake.state === "alive"){
+                    $(snake.body).each(function(index, block){
+                        drawBlock(10*block[posX], 10*block[posY]);
+                    });
+                }
             }
             // draw/update snakes
             var drawSnakes = function(snakes){
@@ -51,7 +70,7 @@
             // connect
             var socket = io.connect("/");
             socket.on('game state', function(data){
-                console.log(data);
+                //console.log(data);
                 clearMap();
                 drawSnakes(data.snakes);
             });
